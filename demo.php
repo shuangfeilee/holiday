1ece235eb3b05c885210ea7063d5f0db401f8f40
shuangfeilee <shuangfeilee@yeah.net>
2020-08-29 18:05:58 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -4,14 +4,21 @@ require_once __DIR__ . '/vendor/autoload.php';
 use mfunc\Holiday;
 
 try {
-	$data = 'https://shuangfeilee.gitee.io/holiday/data.json';
+	// 使用远程数据文件
+	// $data = 'https://shuangfeilee.gitee.io/holiday/data.json';
 	// $holiday = new Holiday($data);
+	
 	$holiday = new Holiday();
+	
+	// 根据年份获取当年节假日 默认当前年
 	$holidays = $holiday->getHolidaysByYear();
-	$isholiday = $holiday->isholiday();
+	
+	// 检测当前日期是否为节假日 默认当天 返回日期信息
+	$result  = $holiday->isholiday();
+	
 	echo '<pre>';
 	print_r($holidays);
-	print_r($isholiday);
+	print_r($result);
 } catch(\Exception $e) {
 	echo $e->getMessage();
 }
